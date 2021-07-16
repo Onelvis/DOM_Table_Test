20b5a1c712ac2db696ae6a8b5ec7fd1af5f7efd9
OnsDR <onelvis96@gmail.com>
2021-07-15 23:17:25 -0400

    Refactoring type formatting

--- js/handlers.js
+++ js/handlers.js
@@ -1,42 +1,32 @@
 
 function addRow(){
 
-	let tableHead = document.getElementById('tableHead');
 
-	/*
-	if(tableHead.childNodes.length === 0){
-		const firstHeaderRow = document.createElement('tr');
-		firstHeaderRow.className = "headersRow";
-		const firstHeader = document.createElement('th');
-		firstHeader.className = "tableHeaders";
-		firstHeaderRow.firstHeader;
-		tableHead.appendChild(firstHeaderRow);
-	}*/
 	const tableBody = document.getElementById('mainTableBody');
 	const newRow = tableBody.insertRow(-1);
-	const headerRow = document.getElementById('headerRow');
 	
 	document.querySelectorAll('#tableHead th').forEach((th)=>{
 		const newCell = newRow.insertCell();
 		const editableDiv = document.createElement('div');
-		//editableDiv.className = "textCell";
 		editableDiv.setAttribute('contenteditable',false);
-		newCell.ondblclick = function(e){
+		newCell.ondblclick = function(){
+			newCell.classList.toggle('selectedTd');
 			editableDiv.setAttribute('contenteditable',!(editableDiv.contentEditable === 'true'));
 			editableDiv.focus();
 		};
 		editableDiv.addEventListener('focusout' , function(e){
 			editableDiv.setAttribute('contenteditable',false);
+			newCell.classList.toggle('selectedTd');
 		});
 		editableDiv.addEventListener('keydown' , function(e){
 			if(e.code === 'Enter'){
 				editableDiv.blur();
 			}
 		});
-		newCell.appendChild(editableDiv);
-		console.log(th);
 
-		editableDiv.className = getTypeClass(th.columnType);
+		newCell.appendChild(editableDiv);
+		applyTypeRules(editableDiv,th.columnType);
+
 	})
 	
 
@@ -44,22 +34,7 @@ function addRow(){
 
 function addColumn(){
 	let tableHead = document.getElementById('tableHead');
-/*
-	if(tableHead.childNodes.length === 1){
-		const headerRow = document.createElement('tr');
-		const columnName = document.getElementById('columnNameInput').value;
-		headerRow.className = "headersRow";
-		headerRow.id = "headerRow";
-		const text = document.createTextNode(columnName);
-		const firstHeader = document.createElement('th');
-		firstHeader.className = "tableHeaders";
-		firstHeader.appendChild(text);
-		headerRow.appendChild(firstHeader);
-		tableHead.appendChild(headerRow);
-	}*/
-
 	//Creating header column
-	const columnName = document.getElementById('columnNameInput').value;
 	const headerRow = document.getElementById('headerRow');
 	const columnHeader = document.createElement('th');
 	const type = document.getElementById('type-select').value;
@@ -76,40 +51,54 @@ function addColumn(){
 	document.querySelectorAll("#mainTableBody tr").forEach( (tr,index) =>{
 		const newCell = document.createElement('td');
 		const editableDiv = document.createElement('div');
-		editableDiv.className = 'textCell';
+		editableDiv.className = "textCell";
 		editableDiv.setAttribute('contenteditable',false);
 		newCell.ondblclick = function(e){
+			newCell.classList.toggle('selectedTd');
 			editableDiv.setAttribute('contenteditable',!(editableDiv.contentEditable === 'true'));
 			editableDiv.focus();
 		};
 		editableDiv.addEventListener('focusout' , function(e){
 			editableDiv.setAttribute('contenteditable',false);
+			newCell.classList.toggle('selectedTd');
 		});
-		editableDiv.addEventListener('keydown' , function(e){
-			if(e.code === 'Enter'){
-				editableDiv.blur();
-			}
-		});
+		
 		newCell.appendChild(editableDiv);
 		tr.appendChild(newCell);
-
-
-		editableDiv.className = getTypeClass(headerRow.childNodes[newCell.cellIndex].columnType);
+		applyTypeRules(editableDiv,headerRow.childNodes[newCell.cellIndex].columnType);
 	});
 
 		
 }
 
-function getTypeClass(type){
+function applyTypeRules(element,type){
 	switch(type){
 		case 'Number':
-			return "numberCell";
+			element.className = 'numberCell';
+			element.addEventListener('keydown' , function(e){
+				if(!(e.keyCode < 14 || e.keyCode == 110 || ((e.keyCode > 47 && e.keyCode <58) ||(e.keyCode > 95 && e.keyCode <106) ))){
+					e.preventDefault();
+				}
+				if(e.code === 'Enter'){
+					element.blur();
+				}
+			});
+			break;
 		case 'Text':
-			return "textCell";
+			element.className = 'textCell';
+			break;
 		case 'Email':
-			return "emailCell";
+			element.className = 'emailCell';
+			element.addEventListener('click',function(e){
+				e.ctrlKey && window.open('mailto:'+element.innerHTML);
+			})
+			break;
 		case 'Url':
-			return "urlCell";
+			element.className = 'urlCell';
+			element.addEventListener('click',function(e){
+				e.ctrlKey && window.open(element.innerHTML);
+			})
+			break;
 	}
 }
 
